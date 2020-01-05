4a3b360fcd4778b8ad55738ef9ff0700f15b8281
George Monsour <gnmonsour@gmail.com>
2020-01-05 13:21:57 -0500

    repository base class

---
+++ repositories/Repository.js
@@ -0,0 +1,84 @@
+const fs = require('fs');
+const crypto = require('crypto');
+const util = require('util');
+const scrypt = util.promisify(crypto.scrypt);
+
+require('dotenv').config();
+const kDLIMITER = process.env.HASH_DELIMITER;
+
+module.exports = class Repository {
+	constructor(filename) {
+		if (!filename) {
+			throw new Error('A filename is needed to create this repository.');
+		}
+		this.filename = filename;
+		try {
+			fs.accessSync(filename);
+		} catch (err) {
+			fs.writeFileSync(this.filename, '[]');
+			console.log('file created');
+		}
+    }
+    
+    async create(attrs) {
+        attrs.id = this.createRandomId();
+        const records = await this.getAll();
+        records.push(attr);
+        await this.writeAll(records);
+    }
+    
+	createRandomSalt() {
+		return crypto.randomBytes(8).toString('hex');
+    }
+    
+	createRandomId() {
+		return crypto.randomBytes(4).toString('hex');
+	}
+
+	async getAll() {
+		return JSON.parse(await fs.promises.readFile(this.filename, { encoding: 'utf8' }));
+	}
+
+	async writeAll(records) {
+		await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 2), { encoding: 'utf8' });
+	}
+
+	async getOne(id) {
+		const records = await this.getAll();
+		return records.find((record) => record.id === id);
+	}
+
+	async delete(id) {
+		const records = await this.getAll();
+		const updatedRecords = records.filter((record) => record.id !== id);
+		await this.writeAll(updatedRecords);
+	}
+
+	async update(id, attrs) {
+		const records = await this.getAll();
+		const record = records.find((record) => record.id === id);
+		if (!record) {
+			throw new Error(`Record with id ${id} not found`);
+		}
+		Object.assign(record, attrs);
+		await this.writeAll(records);
+	}
+
+	async getFirst(filters) {
+		const records = await this.getAll();
+		const keys = Object.keys(filters);
+		for (let record of records) {
+			let isFound = true;
+			for (let key of keys) {
+				if (record[key] !== filters[key]) {
+					isFound = false;
+					break; // break out of the inner loop
+				}
+			}
+			if (isFound) return record;
+		}
+		return undefined;
+	}
+}
+
+
--- repositories/UsersRepository.js
+++ repositories/UsersRepository.js
@@ -5,30 +5,18 @@ const scrypt = util.promisify(crypto.scrypt);
 
 require('dotenv').config();
 const kDLIMITER = process.env.HASH_DELIMITER;
+const Repository = require('./Repository.js');
 
-class UsersRepository {
+class UsersRepository extends Repository {
 	constructor(filename) {
-		if (!filename) {
-			throw new Error('A filename is needed to create this repository.');
-		}
+		super(filename);
 
 		if (!UsersRepository.singleton) {
-			this.filename = filename;
-
-			try {
-				fs.accessSync(filename);
-			} catch (err) {
-				fs.writeFileSync(this.filename, '[]');
-				console.log('file created');
-			}
 			UsersRepository.singleton = this;
 		}
 		return UsersRepository.singleton;
 	}
 
-	async getAll() {
-		return JSON.parse(await fs.promises.readFile(this.filename, { encoding: 'utf8' }));
-	}
 
 	async create(attrs) {
 		attrs.id = this.createRandomId();
@@ -44,55 +32,6 @@ class UsersRepository {
 	async validatedPassword(supplied, saved) {
 		const [ compareWith, savedsalt ] = saved.split(kDLIMITER);
 		return compareWith === (await scrypt(supplied, savedsalt, 64)).toString('hex');
-	}
-
-	async writeAll(records) {
-		await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 2), { encoding: 'utf8' });
-	}
-
-	createRandomId() {
-		return crypto.randomBytes(4).toString('hex');
-	}
-
-	createRandomSalt() {
-		return crypto.randomBytes(8).toString('hex');
-	}
-
-	async getOne(id) {
-		const users = await this.getAll();
-		return users.find((user) => user.id === id);
-	}
-
-	async delete(id) {
-		const users = await this.getAll();
-		const updatedUsers = users.filter((user) => user.id !== id);
-		await this.writeAll(updatedUsers);
-	}
-
-	async update(id, attrs) {
-		const users = await this.getAll();
-		const user = users.find((user) => user.id === id);
-		if (!user) {
-			throw new Error(`User with id ${id} not found`);
-		}
-		Object.assign(user, attrs);
-		await this.writeAll(users);
-	}
-
-	async getFirst(filters) {
-		const users = await this.getAll();
-		const keys = Object.keys(filters);
-		for (let user of users) {
-			let isFound = true;
-			for (let key of keys) {
-				if (user[key] !== filters[key]) {
-					isFound = false;
-					break; // break out of the inner loop
-				}
-			}
-			if (isFound) return user;
-		}
-		return undefined;
 	}
 }
 
